555a6ab81d7f08f5d178d554dfcdba6d9d32b952
Flifer <102440778+Flifer@users.noreply.github.com>
2025-11-18 23:39:19 +0200

    Community section finished

--- src/components/CommunitySection/CommunitySection.jsx
+++ src/components/CommunitySection/CommunitySection.jsx
@@ -5,33 +5,33 @@ function CommunitySection() {
     const { t } = useTranslation();
     return(
         <section className="community-section">
-            <div>
+            <div className="community-section-title">
                 <h2>{t("communitySection.title")}</h2>
-                <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div className="community-section-title-img-container">
+                    <div className='community-section-img1'></div>
+                    <div className='community-section-img2'></div>
+                    <div className='community-section-img3'></div>
                 </div>
             </div>
 
-            <div>
+            <div className="community-section-description">
                 <h3>{t("communitySection.description")}</h3>
-                <div></div>
-                <div>
-                    <div>
+                <div className="community-section-description-img-container"></div>
+                <div className="community-section-description-stats-container">
+                    <div className="community-section-description-stats-item">
                         <h4>{t("communitySection.stats.rating.value")}</h4>
                         <p>{t("communitySection.stats.rating.label")}</p> 
                     </div>
-                    <div>
+                    <div className="community-section-description-stats-item">
                         <h4>{t("communitySection.stats.sessions.value")}</h4>
                         <p>{t("communitySection.stats.sessions.label")}</p>
                         
                     </div>
-                    <div>
+                    <div className="community-section-description-stats-item">
                         <h4>{t("communitySection.stats.areas.value")}</h4>
                         <p>{t("communitySection.stats.areas.label")}</p>
                     </div>
-                    <div>
+                    <div className="community-section-description-stats-item">
                         <h4>{t("communitySection.stats.hours.value")}</h4>
                         <p>{t("communitySection.stats.hours.label")}</p>
                     </div>
